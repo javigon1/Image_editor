1b3df3ea0328a333e79ca372ed1743b00d5c7e91
javigon1 <160635649+javigon1@users.noreply.github.com>
2024-02-22 23:11:52 -0500

    Update ppmtrans.c

--- ppmtrans.c
+++ ppmtrans.c
@@ -23,6 +23,17 @@
 } while (false)
 
 
+struct closure {
+        A2Methods_T methods;
+        A2Methods_UArray2 array2;
+};
+
+struct imageInfo {
+        int rotation, width, height;
+        char *image_name, *mapping, *transformation;
+};
+
+
 void rotate0(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl);
 void rotate90(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl);
 void rotate180(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl);
@@ -30,8 +41,7 @@ void rotate270(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl);
 void flipHorizontal(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl);
 void flipVertical(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl);
 void doTranspose(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl);
-void outputResults();
-void writeTimer(double time_used, char *time_file_name, int rotation);
+void writeTimer(double time_used, char *time_file_name, struct imageInfo);
 
 static void usage(const char *progname)
 {
@@ -43,11 +53,6 @@ static void usage(const char *progname)
         exit(1);
 }
 
-struct closure {
-        A2Methods_T methods;
-        A2Methods_UArray2 array2;
-};
-
 
 int main(int argc, char *argv[])
 {
@@ -55,11 +60,11 @@ int main(int argc, char *argv[])
         int   rotation       = 0;
         int   i;
         bool  file_given     = false;
-
         bool  horizontal     = false;
         bool  vertical       = false;
         bool  transpose      = false;
-
+        char  *mapping       = "row-major";
+        bool  rotation_given = false;
 
         /* default to UArray2 methods */
         A2Methods_T methods = uarray2_methods_plain; 
@@ -73,12 +78,15 @@ int main(int argc, char *argv[])
                 if (strcmp(argv[i], "-row-major") == 0) {
                         SET_METHODS(uarray2_methods_plain, map_row_major, 
                                     "row-major");
+                                    mapping = "row major";
                 } else if (strcmp(argv[i], "-col-major") == 0) {
                         SET_METHODS(uarray2_methods_plain, map_col_major, 
                                     "column-major");
+                                    mapping = "column major";
                 } else if (strcmp(argv[i], "-block-major") == 0) {
                         SET_METHODS(uarray2_methods_blocked, map_block_major,
                                     "block-major");
+                                    mapping = "block major";
                 } else if (strcmp(argv[i], "-rotate") == 0) {
                         if (!(i + 1 < argc)) {      /* no rotate value */
                                 usage(argv[0]);
@@ -90,6 +98,9 @@ int main(int argc, char *argv[])
                                 fprintf(stderr, 
                                         "Rotation must be 0, 90 180 or 270\n");
                                 usage(argv[0]);
+                        }
+                        if (rotation == 0) {
+                                rotation_given = true;
                         }
                         if (!(*endptr == '\0')) {    /* Not a number */
                                 usage(argv[0]);
@@ -124,9 +135,11 @@ int main(int argc, char *argv[])
                         usage(argv[0]);
                 } else {
                         file_given = true;
-                }
+                       }
         }
-
+        // queries populated after that: rotation, mapping major, time_file
+        
+        // open the file to read
         FILE *fp;
         if (!file_given) {
                 fp = stdin;
@@ -140,6 +153,9 @@ int main(int argc, char *argv[])
                 }
         }
 
+        (void)time_file_name;
+
+        // ppm variable to take over the image info read from file.
         Pnm_ppm orig_image = Pnm_ppmread(fp, methods);
         fclose(fp);
         assert(orig_image);
@@ -148,11 +164,11 @@ int main(int argc, char *argv[])
 
         // declarations used for counting time
         CPUTime_T timer = CPUTime_New();
-        double time_used;
 
         // only timing rotations (operations)
         CPUTime_Start(timer);
-        if (rotation == 0) {
+
+        if (rotation == 0 && rotation_given) {
                 new_image = methods->new(methods->width(orig_image), 
                                          methods->height(orig_image),
                                          sizeof(struct Pnm_rgb));
@@ -184,20 +200,40 @@ int main(int argc, char *argv[])
                                          sizeof(struct Pnm_rgb));
                 struct closure infoGet = {methods, new_image};
                 map(orig_image->pixels, flipHorizontal, &infoGet);
-        } else if (vertical) {
+        } 
+
+        if (vertical) {
                 new_image = methods->new(methods->width(orig_image), 
                                          methods->height(orig_image),
                                          sizeof(struct Pnm_rgb));
                 struct closure infoGet = {methods, new_image};
                 map(orig_image->pixels, flipVertical, &infoGet);
-        } else if (transpose) {
+        } 
+        
+        if (transpose) {
                 new_image = methods->new(methods->height(orig_image), 
                                          methods->width(orig_image),
                                          sizeof(struct Pnm_rgb));
                 struct closure infoGet = {methods, new_image};
                 map(orig_image->pixels, doTranspose, &infoGet);
         }
-        time_used = CPUTime_Stop(timer);
+
+        double time_used = CPUTime_Stop(timer);
+
+        if (time_file_name != NULL) {
+                char *transformation = (horizontal) ? "horizontal" : "NO" ;
+                transformation = (vertical) ? "vertical" : "NO" ;
+                transformation = (transpose) ? "transpose" : "NO" ;
+                struct imageInfo image_info = { rotation, 
+                                                methods->width(orig_image),
+                                                methods->height(orig_image),
+                                                argv[argc - 1], 
+                                                mapping, transformation };
+                writeTimer(time_used, time_file_name, image_info);
+        }
+        
+
+        CPUTime_Free(&timer);
 
         methods->free(&orig_image->pixels);
         orig_image->width = methods->width(new_image);
@@ -210,7 +246,18 @@ int main(int argc, char *argv[])
         return 0;
 }
 
-
+/* apply functions start here */
+/*
+ * rotate0
+ * parameter: int i, the column of the current element is applying
+ *            int j, the row of the current element
+ *            a (polymorphoic) UArray2 / UArray2b 
+ *            void, the current element (under the hood was pointed through
+ *                                       At in uarray2b.c or uarray2.c)
+ *            void, the closure
+ * return:    N/A
+ * 
+ */
 void rotate0(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl)
 {
         (void)array2;
@@ -221,20 +268,23 @@ void rotate0(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl)
         *rotated_pixel = *array_pixel;
 }       
 
-
 void rotate90(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl)
 {
         struct closure *info = cl;
         int height = info->methods->height(array2);
         int width = info->methods->width(array2);
 
-        if (height - j - 1 < 0 || height - j - 1 >= height || i < 0 || i >= width) {
-                fprintf(stderr, "Error: Invalid pixel coordinates (%d, %d) -> (%d, %d)\n", i, j, height - j - 1, i);
+        if (height - j - 1 < 0 || height - j - 1 >= height || 
+                                  i < 0 || i >= width) {
+                fprintf(stderr, 
+                        "Error: Invalid pixel coordinates (%d, %d)->(%d, %d)\n",
+                        i, j, height - j - 1, i);
                 exit(EXIT_FAILURE);
         }
 
         Pnm_rgb array_pixel = elem;
-        Pnm_rgb rotated_pixel = info->methods->at(info->array2, height - j - 1, i);
+        Pnm_rgb rotated_pixel = info->methods->at(info->array2, 
+                                                  height - j - 1, i);
         *rotated_pixel = *array_pixel;
 }
 
@@ -246,7 +296,8 @@ void rotate180(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl)
         int width = info->methods->width(array2);
 
         Pnm_rgb array_pixel = elem;
-        Pnm_rgb rotated_pixel = info->methods->at(info->array2, width - i - 1, height - j - 1);
+        Pnm_rgb rotated_pixel = info->methods->at(info->array2, width - i - 1, 
+                                                  height - j - 1);
         *rotated_pixel = *array_pixel;
 }
 
@@ -257,8 +308,11 @@ void rotate270(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl)
         int height = info->methods->height(array2);
         int width = info->methods->width(array2);
 
-        if (j < 0 || j >= height || width - i - 1 < 0 || width - i - 1 >= width) {
-                fprintf(stderr, "Error: Invalid pixel coordinates (%d, %d) -> (%d, %d)\n", i, j, height - j - 1, i);
+        if (j < 0 || j >= height || width - i - 1 < 0 || 
+                                    width - i - 1 >= width) {
+                fprintf(stderr, 
+                        "Error: Invalid pixel coordinates (%d, %d)->(%d, %d)\n",
+                        i, j, height - j - 1, i);
                 exit(EXIT_FAILURE);
         }
 
@@ -300,34 +354,32 @@ void doTranspose(int i, int j, A2Methods_UArray2 array2, void *elem, void *cl)
         *rotated_pixel = *array_pixel;
 }
 
+/* apply functions end */
 
-void outputResults(char* filename)
+/* write time info to a file */
+void writeTimer(double time_used, char *time_file_name, 
+                struct imageInfo image_info)
 {
-        FILE *outputFile;
-        outputFile = fopen(filename, "w");
-                if (!outputFile) {
-                        fprintf(stderr, 
-                                "Error: Cannot open file '%s' for reading.\n", 
-                                argv[argc - 1]);
-                        exit(EXIT_FAILURE);
-                }
-
-
-}
-
-void writeTimer(double time_used, char *time_file_name, int rotation) {
         FILE *time_file = fopen(time_file_name, "a"); // opened in append mode
 
         if (time_file == NULL) {
                 fprintf(stderr, "Error opening file: %s\n", time_file_name);
                 return;
         }
-    
+
+        int pixel_total = image_info.width * image_info.height;
+        double time_per_pixel = time_used / pixel_total;
+
         // Write the timing information to the file
         // may improve by including more image info
-        fprintf(time_file, "Rotation: %d, Time: %.0f nanoseconds\n", 
-                           rotation, time_used);
-        
+        fprintf(time_file, "FILE: %s\nImage contains a width of %d pixels and a"
+                " height of %d pixels\nRotation: %d degree\nTransformation: %s\n"
+                "Mapping: %s \nTime taken: %0.f nanoseconds\nTime taken on each"
+                " pixel: %0.f nanoseconds\n", image_info.image_name, 
+                image_info.width, image_info.height, image_info.rotation, 
+                image_info.transformation, image_info.mapping, time_used, 
+                time_per_pixel);
+
         // Close the file
         fclose(time_file);
 }
